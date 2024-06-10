5cf669e44c944ddfe6b90a1d40409025e0c92e8a
Spas Arnaudov <spasarnaudov@gmail.com>
2024-06-10 12:12:03 +0300

    Used ksp insteat kept

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
 }
 
@@ -73,8 +73,8 @@ dependencies {
 
     //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.50")
-    kapt ("com.google.dagger:hilt-android-compiler:2.50")
-    kapt ("androidx.hilt:hilt-compiler:1.1.0")
+    ksp ("com.google.dagger:hilt-android-compiler:2.50")
+    ksp ("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     //Retrofit
