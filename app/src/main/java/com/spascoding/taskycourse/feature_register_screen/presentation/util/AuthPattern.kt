f6a98663a2cf97fd63e0e442290c9c96d247d3a5
Spas Arnaudov <spasarnaudov@gmail.com>
2024-06-12 16:40:18 +0300

    Made password check more readable

--- app/src/main/java/com/spascoding/taskycourse/feature_register_screen/presentation/util/AuthPattern.kt
+++ app/src/main/java/com/spascoding/taskycourse/feature_register_screen/presentation/util/AuthPattern.kt
@@ -5,27 +5,12 @@ import android.util.Patterns
 object AuthPattern {
     fun name(text: String): Boolean = text.length in 4..50
     fun email(text: String): Boolean = Patterns.EMAIL_ADDRESS.matcher(text).matches()
-    fun password(text: String): Boolean {
-        // Check if the length is at least 9 characters
-        if (text.length < 9) return false
+    fun password(password: String): Boolean {
+        val hasLowerCase = password.any { it.isLowerCase() }
+        val hasUpperCase = password.any { it.isUpperCase() }
+        val hasDigit = password.any { it.isDigit() }
+        val hasValidLength = password.length > 9
 
-        // Initialize flags for each condition
-        var hasLowercase = false
-        var hasUppercase = false
-        var hasDigit = false
-
-        // Iterate through each character in the text
-        for (char in text) {
-            when {
-                char.isLowerCase() -> hasLowercase = true
-                char.isUpperCase() -> hasUppercase = true
-                char.isDigit() -> hasDigit = true
-            }
-            // If all conditions are met, return true
-            if (hasLowercase && hasUppercase && hasDigit) return true
-        }
-
-        // Return false if any condition is not met
-        return false
+        return hasLowerCase && hasUpperCase && hasDigit && hasValidLength
     }
 }
